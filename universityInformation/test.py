084972aaa12d0b030df7d03296f1a1f9bbba2dac
shixiuhai <15256728901@163.com>
2022-10-04 17:06:55 +0800

    更新了test.py测试

--- universityInformation/test.py
+++ universityInformation/test.py
@@ -7,6 +7,25 @@
 # for key in a.keys():
 #     print(key)
 # print(a['www.ahcme.edu.cn'])
-import tldextract
-url="http://zwb.hfut.edu.cn/info/1029/4028.htm"
-print(tldextract.extract(url).domain+ "."+tldextract.extract(url).suffix)
+# import tldextract
+# url="http://zwb.hfut.edu.cn/info/1029/4028.htm"
+# print(tldextract.extract(url).domain+ "."+tldextract.extract(url).suffix)
+from gc import callbacks
+
+
+
+def fun1(response=10):
+    for i in range(response):
+        print("i的结果是: "+str(i))
+        yield get_response(callback=fun1)
+
+
+
+#         fun1()
+# for t in fun1():
+#     print(t)
+def get_response(callback):
+    callback(5)
+
+for t in fun1():
+    print(t)
